589a4c9140589850fd2ef7e12b5500837f09b362
sol peskin <psolchu@gmail.com>
2022-07-16 21:22:31 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar'; 
 
 function App() {
-  const name = "gothics"
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          {name}
-        </p>
-        <a className="App-link" href="https://plataforma.coderhouse.com/" target="_blank" rel="noopener noreferrer">
-          CoderHouse hola
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
 
 export default App;
 
-// // probando
-// const pruebaObjeto ={
-//   prop1: "1",
-//   prop2: "2",
-//   prop3: "3"
-// }
-
-// const { prop1 , prop2 = "nuevoValor"} = pruebaObjeto
-// const {prop3: nuevaVar} = pruebaObjeto
-
-// console.log(pruebaObjeto)
-// console.log(prop1, prop2)
-// console.log(nuevaVar)
-// console.log(pruebaObjeto.prop3)
